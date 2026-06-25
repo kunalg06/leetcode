469745983e4e37ab1aaece21c83988bfb536000d
Kunalg06 <Kunal.gaikwad06@outlook.com>
2026-06-25 14:42:00 +0530

    add explanation for logic

--- Move all 0s to the end.py
+++ Move all 0s to the end.py
@@ -9,10 +9,10 @@ def move_zeros_to_end(arr):
     i = 0
     j = len(arr) - 1
     while i < j:
-        if arr[i] != 0:
+        if arr[i] != 0: #why do we need to check for non-zero elements? Because we want to keep the order of non-zero elements intact and move all zeros to the end. If we find a non-zero element at index i, we simply move the pointer i forward to check the next element.
             i += 1
-        elif arr[j] == 0:
-            j -= 1
+        elif arr[j] == 0: #why do we need to check for zero elements? Because we want to move all zeros to the end. If we find a zero element at index j, we simply move the pointer j backward to check the previous element.
+            j -= 1 
         else:
             arr[i], arr[j] = arr[j], arr[i]
             i += 1
